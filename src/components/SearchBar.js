b2b059ea12065a37b4f90fb80969cd7c3ee4b268
Webdev-DavidM <56081000+Webdev-DavidM@users.noreply.github.com>
2021-10-05 18:19:32 +0100

    search bar created

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,26 +1,27 @@
 import React from 'react';
+import ArrowImg from '../images/icon-arrow.svg';
 
 export default function SearchBar({ searchFunction }) {
   const [userInput, setUserInput] = React.useState('');
   const [error, setError] = React.useState(false);
 
   return (
-    <div data-test='search-bar'>
+    <div data-test='search-bar' className='app__search-bar'>
       <input
         data-test='input'
+        className='app__input'
         type='text'
         value={userInput}
         onChange={(e) => setUserInput(e.target.value)}
       />
-      <button
+      <div
         data-test='search-button'
-        className='app__search-bar'
+        className='app__search-button'
         onClick={() =>
           userInput !== '' ? searchFunction(userInput) : setError(true)
         }>
-        <img src='./images/icon-arrow.svg' alt='' />
-        click
-      </button>
+        <img src={ArrowImg} alt='' />
+      </div>
       {error && <p data-test='error'>Please enter a value</p>}
     </div>
   );
